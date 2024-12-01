d2ac5ece9566cc74cc818caf24f855d0249c926b
afuaagbesi <115745478+afuaagbesi@users.noreply.github.com>
2024-12-01 21:52:14 +0000

    first commit

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ sequelize.sync().then(() => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
